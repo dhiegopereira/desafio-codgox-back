e63e9d566c38de6f5d4c5a574aee80aeecb3748c
studentdeveloperone <dhiego_ti@hotmail.com>
2024-07-21 12:11:14 -0300

    first commit

---
+++ DesafioBackCodgoX.Api/src/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using DesafioBackCodgoX.Application.DTOs;
+using DesafioBackCodgoX.Application.Interfaces;
+using DesafioBackCodgoX.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesafioBackCodgoX.Api;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UsersController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<UserDto>> GetById(string id)
+    {
+        var user = await _userService.GetByIdAsync(id);
+        return Ok(user);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<UserDto>>> GetAll()
+    {
+        var users = await _userService.GetAllAsync();
+        return Ok(users);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add([FromBody] CreateUpdateUserDto createUpdateUserDto)
+    {
+        await _userService.AddAsync(createUpdateUserDto);
+        return Created();
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(string id, [FromBody] CreateUpdateUserDto createUpdateUserDto)
+    {
+        await _userService.UpdateAsync(id, createUpdateUserDto);
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(string id)
+    {
+        await _userService.DeleteAsync(id);
+        return NoContent();
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Pair programming Back End
 
-## Instruçoes para executar o projeto
+## Instruções para executar o projeto
 
 - Inicializar a API
 
@@ -14,7 +14,33 @@ dotnet run
 
 ## Objetivo do teste
 
-Implementar o endpoint de login
+Implementar o endpoint de login com os seguintes requisitos:
+- Deve ser criado endpoint 
+
+> POST /api/login
+
+- Exemplo do payload:
+```json
+{
+    "email": "teste@teste.com",
+    "password": "12345678"
+}
+```
+- Exemplo de resposta:
+```json
+{
+    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImRoaWVn"
+}
+```
+- OBS: No token deve está contido o `id` e `email` do usuário
 
 ---
+
+### Links úteis:
+- [Autenticação e Autorização com JWT no ASP.NET Core](https://chatgpt.com/c/c4ecbec7-88bd-4957-91e5-e2fc9b71f4e0#:~:text=Autentica%C3%A7%C3%A3o%20e%20Autoriza%C3%A7%C3%A3o%20com%20JWT%20no%20ASP.NET%20Core)
+- [Configurando o JWT Bearer Authentication](https://chatgpt.com/c/c4ecbec7-88bd-4957-91e5-e2fc9b71f4e0#:~:text=Configurando%20o%20JWT%20Bearer%20Authentication)
+- [Trabalhando com JWTs no ASP.NET Core](https://chatgpt.com/c/c4ecbec7-88bd-4957-91e5-e2fc9b71f4e0#:~:text=Trabalhando%20com%20JWTs%20no%20ASP.NET%20Core)
+
+---
+
 ![](./swagger.png)
